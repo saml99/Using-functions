98d9fed6355718635510f9e22db592710afa17e4
saml99 <samuel.lewis@gmail.com>
2018-03-06 18:33:40 +1100

    Add files via upload

--- HelloUserWithFunctions.pas
+++ HelloUserWithFunctions.pas
@@ -1,2 +1,13 @@
 program HelloUserWithFuntions;
 uses TerminalUserInput;
+
+procedure Main();
+var
+	name: String;
+	age: Integer;
+	year: Integer;
+	yearBorn: Integer;
+begin
+	name := ReadString('Please enter your name: ');
+	age := ReadInteger('How old are you this year? : ');
+	
